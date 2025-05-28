176658840049e30e428d8ab09609028f37e9aaff
Chris Simmons <chrissimmons@mac.lan>
2025-05-27 17:11:13 -0700

    added openai key retrieval logic

---
+++ distributed_training.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import data_preprocessing as dp
+from helper_module import helper as h
+
+def main():
+    
+    openai_key = h.extract_openai_api_key()
+    
+    return 
+    
